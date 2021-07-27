1a189da50be2f04c4a0392198a33331c491ee566
gandi0330 <gandi0330@naver.com>
2021-07-27 13:29:45 +0900

    django course 29 commit

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -143,6 +143,11 @@ LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
